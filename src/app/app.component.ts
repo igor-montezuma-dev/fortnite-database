cdcd6848cd71ec5cee1e0ebe1232e8af61a014b3
IgorMontezuma20 <igorminranda17@gmail.com>
2024-09-12 17:12:54 -0300

    feat: Create Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { HeaderComponent } from './shared/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fortnite-database';
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+  api: 'https://fortnite-api.com/v2',
+};
